51a40db5e7bbab50087f859918b10623c80f93e6
Travis Jeffery <tj@travisjeffery.com>
2016-02-09 06:30:37 -0500

    -> 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Finicity.Mixfile do
 
   def project do
     [app: :finicity,
-     version: "0.0.1",
+     version: "0.0.2",
      description: "Client library for Finicity.",
      maintainers: ["Travis Jeffery"],
      elixir: "~> 1.2",
