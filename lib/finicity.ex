48fc01bbb123d5d96f29273650f142463c6a66ef
Travis Jeffery <tj@travisjeffery.com>
2016-02-09 05:38:25 -0500

    change api mark some fns as private

--- lib/finicity.ex
+++ lib/finicity.ex
@@ -1,36 +1,6 @@
 defmodule Finicity do
   use HTTPotion.Base
 
-  def process_url(url) do
-    "https://api.finicity.com/aggregation" <> url
-  end
-
-  def process_request_headers(headers) do
-    headers
-    |> Dict.put(:"Content-Type", "application/xml")
-    |> Dict.put(:"Finicity-App-Key", app_key())
-  end
-
-  def process_response_body(body) do
-    body |> Floki.parse
-  end
-
-  def app_token(token) do
-    ["Finicity-App-Token": token]
-  end
-
-  def app_key do
-    System.get_env("FINICITY_APP_KEY")
-  end
-
-  def partner_id do
-    System.get_env("FINICITY_PARTNER_ID")
-  end
-
-  def partner_secret do
-    System.get_env("FINICITY_PARTNER_SECRET")
-  end
-
   def partner_authentication do
     data = {:credentials, nil, [
                {:partnerId, nil, partner_id()},
@@ -40,8 +10,38 @@ defmodule Finicity do
     body |> Floki.Finder.find("token") |> Floki.FlatText.get
   end
 
-  def get_institutions([params: params, token: token]) do
+  def get_institutions(token, params) do
     %{status_code: 200, body: body} = get("/v1/institutions?" <> URI.encode_query(params), [headers: app_token(token)])
     body |> Floki.Finder.find("institutions")
+  end
+
+  defp process_url(url) do
+    "https://api.finicity.com/aggregation" <> url
+  end
+
+  defp process_request_headers(headers) do
+    headers
+    |> Dict.put(:"Content-Type", "application/xml")
+    |> Dict.put(:"Finicity-App-Key", app_key())
+  end
+
+  defp process_response_body(body) do
+    body |> Floki.parse
+  end
+
+  defp app_token(token) do
+    ["Finicity-App-Token": token]
+  end
+
+  defp app_key do
+    System.get_env("FINICITY_APP_KEY")
+  end
+
+  defp partner_id do
+    System.get_env("FINICITY_PARTNER_ID")
+  end
+
+  defp partner_secret do
+    System.get_env("FINICITY_PARTNER_SECRET")
   end
 end
